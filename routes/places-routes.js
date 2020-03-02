0cdc764b27e023d7dc24eab072592c2db7c733a1
Dinesh Maurya <dineshmaurya@hotmail.com>
2020-03-03 00:34:26 +0530

    initial commit

---
+++ routes/places-routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const placeControllers = require('../controllers/places-controllers');
+
+
+const router = express.Router();
+
+
+
+router.get('/:pid',placeControllers.getPlaceById);
+
+router.get('/user/:uid',placeControllers.getPlaceByUserId);
+
+router.post('/',placeControllers.createPlace);
+
+module.exports = router;
